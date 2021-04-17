d20a1baa83889545e3fb4d2f9dcc46d89f10ecc2
perkinsashley26 <perkinsashley26@gmail.com>
2021-04-17 03:06:43 -0500

    Fixed typo

--- persistence/user_models.py
+++ persistence/user_models.py
@@ -10,7 +10,7 @@ class UserModel(db.Model):
     # Database fields
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
-    last_name = db.column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
