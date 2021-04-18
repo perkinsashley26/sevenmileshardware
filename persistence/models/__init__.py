993309f9caa437ccdf330fc7b930332c2b30cd07
perkinsashley26 <perkinsashley26@gmail.com>
2021-04-18 16:06:58 -0500

    Updated requirements and got server running

--- persistence/models/__init__.py
+++ persistence/models/__init__.py
@@ -2,5 +2,5 @@
 from app import Server
 
 # Instantiate only one instance of the server to adhere to singleton pattern
-flask_server = Server(__name__)
+flask_server = Server()
 db = flask_server.db
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """The main driver of the application."""
-from persistence import flask_server
+from persistence.models import flask_server
 
 
 if __name__ == "__main__":
-    flask_server.run(debug=True)
+    flask_server.run(debug=True, host='localhost', port='8080')
